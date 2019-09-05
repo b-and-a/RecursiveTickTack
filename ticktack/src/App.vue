ba33153dd5b7461c65a6169395aa25053861383f
banda taito <taito.banda@ulsystems.co.jp>
2019-09-05 11:31:06 +0900

    first commit

---
+++ ticktack/src/App.vue
@@ -0,0 +1,86 @@
+<template>
+  <split-panels :config="splitPanelsConfig3" />
+</template>
+
+<script>
+import SplitPanels from './components/SplitPanels.vue'
+
+export default {
+  name: 'app',
+  components: {
+    SplitPanels
+  },
+  data() {
+    return {
+      splitPanelsConfig1: {
+        direction: 'horizontal',
+        panels: [{
+          id: 'p00',
+          url: 'a.html',
+          panels: []
+        }, {
+          id: 'p01',
+          url: 'b.html',
+          panels: []
+        }, {
+          id: 'p02',
+          url: 'c.html',
+          panels: []
+        }]
+      },
+      splitPanelsConfig2: {
+        id: 'p00',
+        url: 'a.html',
+        panels: []
+      },
+      splitPanelsConfig3: {
+        direction: 'horizontal',
+        panels: [{
+          id: 'p00',
+          url: 'a.html',
+          panels: []
+        }, {
+          id: 'p01x',
+          direction: 'vertical',
+          panels: [{
+            id: 'p01-p00',
+            url: 'a.html',
+            panels: []
+          }, {
+            id: 'p01-p01',
+            url: 'a.html',
+            panels: []
+          }, {
+            id: 'p1-p02x',
+            direction: 'horizontal',
+            panels: [{
+              id: 'p01-p02-p00',
+              url: 'a.html',
+              panels: []
+            }, {
+              id: 'p01-p02-p01',
+              url: 'b.html',
+              panels: []
+            }]
+          }]
+        }, {
+          id: 'p02',
+          url: 'c.html',
+          panels: []
+        }]
+      },
+    };
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ ticktack/src/components/SplitPanels.vue
@@ -0,0 +1,61 @@
+<template>
+  <div v-if="(config.panels === null) || (config.panels.length === 0)"
+    style="height:100%; width:100%">
+    <iframe v-bind:src="config.url" />
+  </div>
+  <splitpanes v-else-if="(config.direction === 'horizontal')"
+    class="default-theme"
+    style="height:100%; width:100%"
+    horizontal>
+    <span v-for="p in config.panels" :key="p.id">
+      <split-panels :config="p" />
+    </span>
+  </splitpanes>
+  <splitpanes v-else-if="(config.direction === 'vertical')"
+    class="default-theme"
+    style="height:100%; width:100%"
+    vertical>
+    <span v-for="p in config.panels" :key="p.id">
+      <split-panels :config="p" />
+    </span>
+  </splitpanes>
+  <div v-else></div>
+</template>
+
+<script>
+import SplitPanels from './SplitPanels.vue'
+import Splitpanes from 'splitpanes'
+import 'splitpanes/dist/splitpanes.css'
+
+export default {
+  name: 'SplitPanels',
+  components: {
+    SplitPanels,
+    Splitpanes
+  },
+  props: {
+    config: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
